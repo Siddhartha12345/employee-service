e691c49db4532d98cbabe4d68dad4c8bde96b6c9
Siddhartha <siddharthapaul101@gmail.com>
2024-03-18 10:38:16 +0530

    project rename

--- src/main/java/com/employee/EMSEmployeeServiceApplication.java
+++ src/main/java/com/employee/EMSEmployeeServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class EmployeeServiceApplication {
+public class EMSEmployeeServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeServiceApplication.class, args);
+		SpringApplication.run(EMSEmployeeServiceApplication.class, args);
 	}
 
 }
